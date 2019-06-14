fabee120a7e217c4caf993bd585eb58d7dcccacc
edelkas <edlucasma@gmail.com>
2019-06-14 04:20:01 +0200

    added some more masks

--- generatePalette.rb
+++ generatePalette.rb
@@ -5,7 +5,7 @@ include ChunkyPNG::Color
 
 dir = '.steam/steam/steamapps/common/N++/NPP/Palettes'
 palettes = Dir.entries(dir).reject{ |f| f == '.' || f == '..' }.sort_by(&:downcase)
-print(palettes)
+print(palettes.to_s)
 names = ["ninja", "entityMine", "entityGold", "entityDoorExit",
   "entityDoorExitSwitch", "entityDoorRegular", "entityDoorLocked",
   "entityDoorTrap", "entityLaunchPad", "entityOneWayPlatform",
@@ -21,9 +21,9 @@ palettes.each_with_index{ |palette, y|
     file = File.binread(dir + "/" + palette + "/" + name)
     width = file[12..13].reverse.unpack('H*')[0].to_i(16)
     colors = width / 64
+    initial = 18 + 4 * 32 * width + 4 * 32
+    step = 4 * 64
     (0..colors - 1).each{ |i|
-      initial = 18 + 4 * 32 * width + 4 * 32
-      step = 4 * 64
       color_code = file[initial + step * i .. initial + step * i + 3]
       color = (color_code[0..2].reverse + color_code[3]).unpack('H*')[0]
       output[x,y] = ChunkyPNG::Color.from_hex(color)
